1061229e3932cad42278efa4b0fb537b7ed39f7d
keithchasen <keithchasen89@gmail.com>
2024-02-22 21:16:52 +0100

    changed top and bottom map images->fixed houses layers / show npc quest marker when hero is close enough

--- src/NPC.js
+++ src/NPC.js
@@ -4,6 +4,8 @@ class NPC extends Person {
 
     player = null;
 
+    showQuest = false;
+
     constructor(name, color) {
         super()
         this.name = name;
@@ -15,6 +17,22 @@ class NPC extends Person {
     }
 
     calculatePosition() {
+
+        const npcX = this.position.x;
+        const npcY = this.position.y;
+
+        const playerX = ~~ (this.player.position.x / this.squareSize);
+        const playerY = ~~ (this.player.position.y / this.squareSize);
+
+        if (
+            (npcX == playerX && (npcY === playerY-1 || npcY === playerY+1)) ||
+            (npcY == playerY && (npcX === playerX-1 || npcX === playerX+1))
+        ) {
+            this.showQuest = true;
+        } else if (this.showQuest) {
+            this.showQuest = false;
+        }
+
         // initially place npcs in the middle of the screen as the hero and the map
         // this.cnv.width / 2
         // this.cnv.height / 2
@@ -39,6 +57,18 @@ class NPC extends Person {
         if (!this.ctx) return ;
 
         const { x, y } = this.calculatePosition();
+
+        if (this.showQuest) {
+            this.ctx.beginPath();
+            this.ctx.rect(
+                x+5,
+                y-15,
+                this.w - 10,
+                this.h - 5
+            );
+            this.ctx.fillStyle = 'yellow';
+            this.ctx.fill();
+        }
         
         this.ctx.beginPath();
         this.ctx.rect(
